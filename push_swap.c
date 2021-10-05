ef3fc9c9301ae511ed0c379020ab32fea4f53f39
Jennine Macmillan <jmacmill@am-k4.msk.21-school.ru>
2021-10-05 19:25:55 +0300

    nakonezto

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,12 @@
 /*   By: jmacmill <jmacmill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 20:12:12 by jmacmill          #+#    #+#             */
-/*   Updated: 2021/10/04 22:18:06 by jmacmill         ###   ########.fr       */
+/*   Updated: 2021/10/05 19:25:41 by jmacmill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
 int	ft_atoi(const char *str)
 {
@@ -58,17 +59,17 @@ int	ft_ischar(char *str)
 	a = 0;
 	while (str[a] != '\0')
 	{
-		if (!ft_isdigit(str[a]))
+		if (!ft_isdigit(str[a]) || str[a] != '-' || str[a] != '+')
 			return (1);
 		a++;
 	}
 	return (0);
 }
 
-void	init_stack(t_stack *a, int num)
+t_stack	*init_stack(t_stack *a, int num)
 {
 	t_stack	*tmp;
-	t_stack	*next;
+	void	*p;
 
 	if (a == NULL)
 	{
@@ -77,18 +78,22 @@ void	init_stack(t_stack *a, int num)
 		a->order = 0;
 		a->flag = 0;
 		a->next = a;
+		return (a);
 	}
 	else
 	{
 		tmp = (t_stack *)malloc(sizeof(t_stack));
-		next = a->next;
+		p = a->next;
 		a->next = tmp;
 		tmp->value = num;
 		tmp->order = 0;
 		tmp->flag = 0;
-		tmp->next = next;
+		tmp->next = p;
 	}
+	return (a);
 }
+
+
 
 int	ft_strchr_ps(char **str)
 {
@@ -110,13 +115,28 @@ int	ft_strchr_ps(char **str)
 	return (1);
 }
 
-void	check_errors(char **argv, t_stack *a)
+void	print_list(t_stack *a)
+{
+	t_stack *head;
+
+	head = a;
+	while (head->next != a)
+	{
+		printf("value: %d\n", head->value);
+		printf("next value: %d\n", head->next->value);
+		head = head->next;
+	}
+	printf("value: %d\n", head->value);
+		printf("next value: %d\n", head->next->value);
+}
+
+void	check_errors(int argc, char **argv, t_stack *a)
 {
 	int			i;
 	long int	num;
 	
-	i = 1;
-	while (argv[i])
+	i = argc - 1;
+	while (i)
 	{
 		num = ft_atoi(argv[i]);
 		if (num > 2147483647 || num < -2147483648)
@@ -128,29 +148,65 @@ void	check_errors(char **argv, t_stack *a)
 		}
 		else if (!ft_isdigit(*argv[i]))
 			get_error();
-		if (!ft_strchr_ps(argv))
+		if (!ft_ischar(argv[i]))
 			get_error();
-		init_stack(a, num);
-		i++;
+		a = init_stack(a, num);
+		i--;
+		if (i == 0)
+			a = a->next;
+	}
+	print_list(a);
+}
+
+int		ft_check(t_stack *a, int value, int len)
+{
+	t_stack	*tmp;
+
+	tmp = a;
+	while(len)
+	{
+		if (tmp->value == value)
+			return (1);
+		tmp = tmp->next;
+		len--;
+	}
+	return (0);
+}
+
+void	check_duplicates(t_stack *a)
+{
+	t_stack	*tmp;
+	int		len;
+
+	tmp = a;
+	len = 0;
+	while (tmp->next != a)
+	{
+		if (ft_check(a, tmp->value, len))
+			get_error();
+		tmp = tmp->next;
+		len++;
 	}
 }
 
-void	push_swap(char **argv, t_stack *a)
+void	push_swap(int argc, char **argv, t_stack *a)
 {
-	check_errors(argv, a);
-	// check_duplicates();
+	check_errors(argc, argv, a);
+	check_duplicates(a);
 	// check_sort();
 	// algos();
 }
 
-int	main(int argc, char **argv)
+// int	main(int argc, char **argv)
+int	main()
 {
 	t_stack	*a;
 	t_stack	*b;
 
 	a = NULL;
 	b = NULL;
+	char *argv[4] = {"x", "1", "2", "3"};
 	if (argc > 2)
-		push_swap(argv, a);
+		push_swap(argc, argv, a);
 	return (0);
 }
